18a284a3e18541921169c16687d5d341bc2f9714
Fernanda de Jesus <fernandabussular@gmail.com>
2022-06-25 00:11:50 +0000

    Structure Chatrooms Controller

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,44 @@
 class ChatroomsController < ApplicationController
+  def index
+    @chatrooms = Chatroom.all
+  end
+
+  def new
+    @chatroom = Chatroom.new
+  end
+
+  def create
+    @chatroom = Chatroom.create(chatroom_params)
+    if @chatroom.save
+      redirect_to chatroom_path(@chatroom)
+    else
+      render :new
+    end
+  end
+
   def show
-    @chatroom = Chatroom.find(params[:id])
     @message = Message.new
+  end
+
+  def edit;end
+
+  def update
+    @chatroom.update(chatroom_params)
+    redirect_to chatroom_path(@chatroom)
+  end
+
+  def destroy
+    @chatroom.destroy
+    redirect_to chatrooms_path
+  end
+
+  private
+
+  def set_params
+    @chatroom = Chatroom.find(params[:id])
+  end
+
+  def chatroom_params
+    params.(:chatroom).permit(:name)
   end
 end
