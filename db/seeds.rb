063255b67fd29a83dc286634450d712a1be9dbfa
Fernanda de Jesus <fernandabussular@gmail.com>
2022-07-25 03:59:46 +0000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,7 @@ User.create(
 )
 Chatroom.create(
   name: "SQL"
+  Chatroom.photo.attach(io: File.open('app/assets/images/2.jpg'), filename: '2.jpg', content_type: 'image/jpg')
 )
 
 puts "End of Seed!"
